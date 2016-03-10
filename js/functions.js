6f2ac455f057b9dc183bc1cf2aa867c6604cdc8b
webkey <webkey_i@ukr.net>
2016-03-10 19:37:14 +0300

    of 03

--- js/functions.js
+++ js/functions.js
@@ -4,8 +4,17 @@ function placeholderInit(){
 }
 /* placeholder end */
 
+/*sticky layout*/
+function stickyLayout(){
+	$(".sticky-js").stick_in_parent({
+		'parent': '.wrapper'
+	});
+}
+/*sticky layout end*/
+
 /** ready/load/resize document **/
 
 $(document).ready(function(){
 	placeholderInit();
+	stickyLayout();
 });
