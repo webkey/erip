c541d9c3806729397ce01c1a7051b52c8a086f73
Serhii <serhiy.ilchenko@gmail.com>
2016-03-11 03:11:49 +0300

    hm 01

--- js/functions.js
+++ js/functions.js
@@ -6,15 +6,58 @@ function placeholderInit(){
 
 /*sticky layout*/
 function stickyLayout(){
-	$(".sticky-js").stick_in_parent({
-		'parent': '.wrapper'
-	});
+	var $stickyJs = $(".sticky-js");
+	if ($stickyJs.length) {
+		$stickyJs.stick_in_parent({
+			//'parent': '.wrapper'
+		});
+	}
 }
 /*sticky layout end*/
+
+/*show form search */
+function showFormSearch(){
+	var searchForm = $('.search-form__header');
+	if(!searchForm.length){ return; }
+
+	var $body = $('body');
+	$body.on('click', '.btn-search', function(){
+		var $currentBtnOpen = $(this);
+		var $currentWrap = $currentBtnOpen.closest('.header-options');
+		var $searchFormContainer = $currentWrap.find('.js-search-form');
+
+		var $searchForm = $searchFormContainer.find('form');
+		if ( $searchForm.find('input:not(:submit)').val().length && $searchFormContainer.is(':visible') ){
+			$searchForm.submit();
+			return;
+		}
+
+		if ($currentWrap.hasClass('form-opened')){
+			closeSearchForm($searchFormContainer,$('.header-options'));
+			return;
+		}
+
+		$currentWrap.addClass('form-opened');
+		$searchFormContainer.find('input[type="search"], input[type="text"]').trigger('focus');
+	});
+
+	$body.on('click', '.js-btn-search-close', function(){
+		var $searchFormContainer = $(this).closest('.js-search-form');
+		$searchFormContainer.find('input:not(:submit)').val('');
+
+		closeSearchForm($searchFormContainer,$('.header-options'));
+	});
+
+	function closeSearchForm(form, wrapper){
+		form.closest(wrapper).removeClass('form-opened')
+	}
+}
+/*show form search end*/
 
 /** ready/load/resize document **/
 
 $(document).ready(function(){
 	placeholderInit();
 	stickyLayout();
+	showFormSearch();
 });
