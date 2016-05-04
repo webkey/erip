5ac442c411c08ff999eec696f761ab4304e7bfdf
webkey <webkey_i@ukr.net>
2016-05-04 17:18:20 +0300

    of 70

--- js/functions.js
+++ js/functions.js
@@ -791,6 +791,26 @@ function equalHeightInit(){
 /*equalHeight end*/
 
 /* tabs */
+function refreshSelectInTabs(select) {
+	$.each(select, function (i, el) {
+		var checked = $(el).multiselect('getChecked');
+		var flag = true;
+		if (!checked.length) {
+			flag = false
+		}
+		if (!flag) {
+			$(el).multiselect('uncheckAll');
+			$(el)
+				.multiselect('widget')
+				.find('.ui-state-active')
+				.removeClass('ui-state-active')
+				.find('input')
+				.removeAttr('checked');
+		}
+		$(el).multiselect('close');
+	});
+}
+
 function tabs() {
 	var $helpfulTabs = $('.helpful');
 	if ($helpfulTabs) {
@@ -803,11 +823,15 @@ function tabs() {
 			animation: 'fade', // slide
 			duration: 300, // default 500
 			animationQueue: true,
-			scrollToAccordion: true
+			scrollToAccordion: true,
 			//scrollToAccordionOffset: true
-			//activate: function(e, tab) {
-			//	console.log(tab);
-			//},
+			activate: function() {
+				var select = $(this).find('select');
+
+				if (DESKTOP && select.length) {
+					refreshSelectInTabs(select);
+				}
+			}
 			//activateState: function(e, state) {
 			//	console.log(state);
 			//}
@@ -825,14 +849,18 @@ function tabs() {
 			animation: 'fade', // slide
 			duration: 300, // default 500
 			animationQueue: true,
-			scrollToAccordion: true
+			scrollToAccordion: true,
 			//scrollToAccordionOffset: true
-			//activate: function(e, tab) {
-			//	console.log(tab);
-			//},
-			//activateState: function(e, state) {
-			//	console.log(state);
-			//}
+			activate: function() {
+				var select = $(this).find('select');
+
+				if (DESKTOP && select.length) {
+					refreshSelectInTabs(select);
+				}
+			}
+			// activateState: function(e, state) {
+			//
+			// }
 		});
 	}
 }
@@ -1261,6 +1289,104 @@ function multiSearchInit() {
 }
 /*multi search end*/
 
+/* multiselect init */
+// add ui position add class
+function addPositionClass(position, feedback, obj){
+	removePositionClass(obj);
+	obj.css( position );
+	obj
+		.addClass( feedback.vertical )
+		.addClass( feedback.horizontal );
+}
+// add ui position remove class
+function removePositionClass(obj){
+	obj.removeClass('top');
+	obj.removeClass('bottom');
+	obj.removeClass('center');
+	obj.removeClass('left');
+	obj.removeClass('right');
+}
+function customSelect(select){
+	if ( select.length ) {
+		selectArray = new Array();
+		select.each(function(selectIndex, selectItem){
+			var placeholderText = $(selectItem).attr('data-placeholder');
+			var flag = true;
+			if ( placeholderText === undefined ) {
+				placeholderText = $(selectItem).find(':selected').html();
+				flag = false;
+			}
+			var classes = $(selectItem).attr('class');
+			selectArray[selectIndex] = $(selectItem).multiselect({
+				header: false,
+				height: 'auto',
+				minWidth: 50,
+				selectedList: 1,
+				classes: classes,
+				multiple: false,
+				noneSelectedText: placeholderText,
+				show: ['fade', 300],
+				hide: ['fade', 300],
+				create: function(event){
+					var select = $(this);
+					var button = $(this).multiselect('getButton');
+					var widget = $(this).multiselect('widget');
+					button.wrapInner('<span class="select-inner"></span>');
+					button.find('.ui-icon').append('<i class="arrow-select"></i>')
+						.siblings('span').addClass('select-text');
+					widget.find('.ui-multiselect-checkboxes li:last')
+						.addClass('last')
+						.siblings().removeClass('last');
+					if ( flag ) {
+						$(selectItem).multiselect('uncheckAll');
+						$(selectItem)
+							.multiselect('widget')
+							.find('.ui-state-active')
+							.removeClass('ui-state-active')
+							.find('input')
+							.removeAttr('checked');
+					}
+				},
+				selectedText: function(number, total, checked){
+					var checkedText = checked[0].title;
+					return checkedText;
+				},
+				position: {
+					my: 'left top',
+					at: 'left bottom',
+					using: function( position, feedback ) {
+						addPositionClass(position, feedback, $(this));
+					}
+				}
+			});
+		});
+		$(window).resize(selectResize);
+	}
+}
+function selectResize(){
+	if ( selectArray.length ) {
+		$.each(selectArray, function(i, el){
+			var checked = $(el).multiselect('getChecked');
+			var flag = true;
+			if ( !checked.length ) {
+				flag = false
+			}
+			$(el).multiselect('refresh');
+			if ( !flag ) {
+				$(el).multiselect('uncheckAll');
+				$(el)
+					.multiselect('widget')
+					.find('.ui-state-active')
+					.removeClass('ui-state-active')
+					.find('input')
+					.removeAttr('checked');
+			}
+			$(el).multiselect('close');
+		});
+	}
+}
+/* multiselect init end */
+
 /** ready/load/resize document **/
 
 $(document).ready(function(){
@@ -1278,13 +1404,16 @@ $(document).ready(function(){
 	terminalsSwitcherInit();
 	actionsLayout();
 	asidePanelSwitcher();
-	tabs();
 	mapMainInit();
 	lightboxPopup();
 	buttonsFromBehavior();
 	stickyLayout();
 	multiAccordionInit();
 	multiSearchInit();
+	if(DESKTOP){
+		customSelect($('.select select'));
+	}
+	tabs();
 });
 
 $(window).load(function () {
