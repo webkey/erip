a1fe7f3a3faa814ca179a7dffc51b052626f71ba
webkey <webkey_i@ukr.net>
2016-05-11 16:23:46 +0300

    of 73

--- js/functions.js
+++ js/functions.js
@@ -1111,6 +1111,8 @@ function lightboxPopup(){
 		mainClass: 'mfp-fade',
 		removalDelay: 160,
 		preloader: false,
+		tClose: 'Закрыть (Esc)',
+		tLoading: 'Загрузка...',
 
 		fixedContentPos: false,
 		callbacks:{
@@ -1120,6 +1122,36 @@ function lightboxPopup(){
 		}
 	}).on('click', function () {
 		$(this).addClass('mfp-opened');
+	});
+
+	$('.gallery-zoom').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true
+			// titleSrc: function(item) {
+			// 	return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
+			// }
+		},
+		tClose: 'Закрыть (Esc)',
+		tLoading: 'Загрузка...',
+		gallery: {
+			enabled: true,
+			tPrev: 'Предыдущий',
+			tNext: 'Следующий',
+			tCounter: '<span class="mfp-counter">%curr% из %total%</span>'
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+			opener: function(element) {
+				return element.find('img');
+			}
+		}
+
 	});
 }
 /*lightbox popup end*/
